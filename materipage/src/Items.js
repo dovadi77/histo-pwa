09c0a713f90b04b1cd782e46e90af75415d101a6
kevinnath25 <73455775+kevinnath25@users.noreply.github.com>
2021-11-30 22:45:21 +0700

    materi page

---
+++ materipage/src/Items.js
@@ -0,0 +1,25 @@
+const items = [
+    {
+        id: 1,
+        name: "BBQ",
+        desc: "BBQ sauce with seasoned grilled chicken",
+        img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.chicken-lovers.b954bbe4c3fed2d6e1bdb2953902be9e.1.jpg?width=300",
+        price: 12.5
+    },
+    {
+        id: 2,
+        name: "Supreme",
+        desc: "Italian sausage, ham, black olive",
+        img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.super-supreme.21f3260bdd33813f550cd523fbd8d073.1.jpg?width=300",
+        price: 14.5
+    },
+    {
+        id: 3,
+        name: "Canadian",
+        desc: "Bacon, ham sausage",
+        img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.canadian.67c22dc316f702bf4d992c667d22fbf0.1.jpg?width=300",
+        price: 12.5
+    }
+];
+
+export default items;
