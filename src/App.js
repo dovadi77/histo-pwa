8562e1d9dc359ad91781c8b2a7d86bee3f1833fe
pieteryonathan <pieteryonathan20@gmail.com>
2021-12-05 21:31:48 +0700

    feat:editprofilepage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/login';
 import Register from './components/register';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Profile from './components/profile';
+import Edit_Profile from './components/edit_profile';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/profile" element={ <Profile />} /> 
+        <Route path="/edit_profile" element={<Edit_Profile />} />
       </Routes>
     </Router>
   );
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,10 +1,14 @@
-import {Grid, Paper, TextField} from "@material-ui/core";
+import {Grid, Paper, TextField, Button} from "@material-ui/core";
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
   
+    const navigate = useNavigate();
+
     const paperStyle = { padding: 20, height: '95vh', width: 355, margin: "0 auto" }
     const txtField = { margin: '15px 0' }
+    const btnStyle = { margin: '30px 0' }
 
     return <div> 
         <Grid>
@@ -13,9 +17,12 @@ function Profile() {
                 <h2>Halaman Profil</h2>
             </Grid>
             <label> Nama </label>
-            <TextField style={txtField} placeholder='Pieter Yonathan' fullWidth />
+                <TextField disabled
+                    id="outlined-disabled" style={txtField} placeholder='Pieter Yonathan' fullWidth />
             <label> Email </label>
-                <TextField style={txtField} placeholder='pieteryonathan@gmail.com' fullWidth  />
+                <TextField disabled
+                    id="outlined-disabled" style={txtField} placeholder='pieteryonathan@gmail.com' fullWidth  />
+                <Button onClick={() => { navigate('/edit_profile') }} type='submit' color='primary' fullWidth variant='contained' style={btnStyle}>Ubah</Button>
         </Paper>
     </Grid>
 
