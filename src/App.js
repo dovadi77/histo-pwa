5a78b9152f7c34c127db3aa2d476bb7076a89667
pieteryonathan <pieteryonathan20@gmail.com>
2021-12-06 12:17:31 +0700

    fear: speech recognition

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from './components/register';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Profile from './components/profile';
 import Edit_Profile from './components/edit_profile';
+import Speech_To_Text from './components/speech_to_text';
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
         <Route path="/login" element={<Login/>} />
         <Route path="/profile" element={ <Profile />} /> 
         <Route path="/edit_profile" element={<Edit_Profile />} />
+        <Route path="/game" element={<Speech_To_Text />} />
+
       </Routes>
     </Router>
   );
--- src/components/speech_to_text.js
+++ src/components/speech_to_text.js
@@ -1,1 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {Button, Grid, Paper, TextField } from "@material-ui/core";
+import SpeechRecognition, {useSpeechRecognition} from "react-speech-recognition";
+ 
+function Speech_To_Text() {
+    const { transcript, resetTranscript } = useSpeechRecognition()
+
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()){
+    return null
+    }
+
+    const paperStyle1 = { padding: 20, height: '40vh', width: 300, margin: "20px auto" }
+    const paperStyle2 = { padding: 20, height: '30vh', width: 300, margin: "30px auto" }
+    const btnStyleStart = { padding: 20, height: '7vh', width: 170, margin: "20px 60px"}
+    const btnStyleStop = { padding: 20, height: '7vh', width: 170, margin: "20px 120px" }
+    const btnStyleReset = { padding: 20, height: '7vh', width: 170, margin: "20px 70px" }
+    return (
+        <div>
+            <Grid>
+                <Paper elevation={10} style={paperStyle1}>
+                    <Grid align='center'>
+                        <h2>Teks Proklamasi</h2>
+                    </Grid>
+                    <label> Kami bangsa Indonesia dengan ini menyatakan Kemerdekaan Indonesia. Hal-hal yang mengenai pemindahan kekuasaan dan lain lain, di selenggarakan dengan cara seksama dan dalam tempo yang sesingkat-singkatnya.  </label>
+                </Paper>
+                <Paper elevation={10} style={paperStyle2}>
+                    <Grid align='center'>
+                        <h2>Hasil Input</h2>
+                    </Grid>
+                    <p>{transcript}</p>
+                </Paper>
+                <Button onClick={SpeechRecognition.startListening({continuous: true})}
+                type='submit' 
+                color='primary' 
+                variant='contained' 
+                style={btnStyleStart}>Start</Button>
+                <Button onClick={SpeechRecognition.stopListening}
+                    type='submit'
+                    color='primary'
+                    variant='contained'
+                    style={btnStyleStop}>Stop</Button>
+                <Button onClick={resetTranscript}
+                    type='submit'
+                    color='primary'
+                    variant='contained'
+                    style={btnStyleReset}>Reset</Button>
+            </Grid>
+        </div>
+    )
+}
+
+export default Speech_To_Text;
