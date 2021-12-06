b515e47f1778df413d3589b2ebe95725f4f62caf
pieteryonathan <pieteryonathan20@gmail.com>
2021-12-06 11:25:00 +0700

    feat: Speech to text

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/login';
 import Register from './components/register';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Profile from './components/profile';
+import Speech_to_text from './components/speech_to_text';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/profile" element={ <Profile />} /> 
+        <Route path="/game" element={ <Speech_to_text />} />
       </Routes>
     </Router>
   );
---
+++ src/components/speech_to_text.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+
+function Speech_to_text(){
+    return (
+    <div>
+        <div>
+            <h2>Current Note</h2>
+            <button>Start/Stop</button>
+        </div>
+        <div>
+            <h2>Notes</h2>
+        </div>
+    </div>
+    )
+}
+
+export default Speech_to_text;
